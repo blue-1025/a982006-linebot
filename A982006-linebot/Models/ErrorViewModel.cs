c78653ed22c85ed0fa55fc896e612819a0e407b0
blue-1025 <b0909399980@gmail.com>
2023-05-23 15:45:57 +0800

    init

---
+++ A982006-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982006_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
